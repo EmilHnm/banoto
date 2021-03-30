2e95fd062cd8f20d3b0cdecd685a15dab91faf7e
Hoa <hoangominh01@gmail.com>
2021-03-30 20:42:59 +0700

    master

--- javascript/index.js
+++ javascript/index.js
@@ -2,8 +2,10 @@ window.onscroll = function() {scrollFunction()}
 function scrollFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("back-to-top").style.display = "block";
+        document.getElementById("navbar").style.position = "fixed";
     } else {
         document.getElementById("back-to-top").style.display = "none";
+        document.getElementById("navbar").style.position = "";
     }
 }
 function backToTop() {
